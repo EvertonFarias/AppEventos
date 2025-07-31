306bc9b5b3a16ae1181506f5dc93e22f50c0141f
Everton Farias <evertondefarias115@gmail.com>
2025-07-31 10:08:39 -0300

    ok

---
+++ src/main/java/com/example/inovaTest/controllers/FileController.java
@@ -0,0 +1,39 @@
+package com.example.inovaTest.controllers;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/files/event-photos")
+public class FileController {
+
+    @PreAuthorize("isAuthenticated()") // Garante que só usuários autenticados podem acessar
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getEventPhoto(@PathVariable String filename) {
+        try {
+            Path filePath = Paths.get(System.getProperty("user.dir"), "uploads", "event-photos", filename);
+            Resource resource = new UrlResource(filePath.toUri());
+            if (!resource.exists() || !resource.isReadable()) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG) // ou detectar dinamicamente
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
